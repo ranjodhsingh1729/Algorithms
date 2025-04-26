65d2e24f732758520bf61b534c55d6de2b48740a
Ranjodh Singh <ranjodhsingh1729@gmail.com>
2025-04-26 12:03:13 +0530

    Added Disjoint Set Union

---
+++ Disjoint Set Union.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+using namespace std;
+typedef long long ll;
+typedef array<int, 3> triple;
+const int MAXN = 2e5+1;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+
+
+struct DSU {
+    int n;
+    vector<int> P;
+
+    DSU(int n) {
+        this->n = n;
+        for (int i = 0; i < n; i++)
+            P.push_back(i);
+    }
+
+    void union_set(int a, int b) {
+        a = find_set(a);
+        b = find_set(b);
+        if (a != b)
+            P[b] = a;
+    }
+
+    int find_set(int a) {
+        if (P[a] == a)
+            return a;
+        else
+            return P[a] = find_set(P[a]);
+    }
+};
+
+
+int main() {
+    return 0;
+}
